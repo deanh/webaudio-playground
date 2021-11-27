efa41234f22fe1ebc1e8ffbb6e6579e350686717
Dean Hudson <deh@client3618.lan>
2021-11-27 02:22:39 +0100

    Some project structure tweaks. Nothing major.

--- src/app.ts
+++ src/app.ts
@@ -3,34 +3,34 @@ import Simple from "./simple";
 import Noise from "./noise"
 
 let aCtx = new AudioContext()
-let m1 = new Metro(aCtx, [1300, 300, 300]);
+let m1 = new Metro(aCtx, [500, 750, 500, 500, 750]);
 
-let sample = new Simple("./rw_auto_10-006.mp3", aCtx, 0.5);
+let sample = new Simple("./rw_auto_10-011.mp3", aCtx, 800);
 
-sample.getFile().then((s) => {
+sample.getFile().then((s: Simple) => {
     m1.addEvery(s);
 });
 
 /* m1.addEvery({
-    stopTime: 0.3,
+    stopTime: 2,
     gen: (ctx) => {
         let osc = ctx.createOscillator();
-        osc.frequency.value = Math.floor(Math.random() * 200 ) + 200;
+        osc.frequency.value = Math.floor(Math.random() * 20) * 10 + 40;
         osc.connect(ctx.destination);
 
         return osc;
     }
 }); */
 
-let sample2 = new Simple("./rw_auto_10-004.mp3", aCtx, 1);
+/* let sample2 = new Simple("./rw_auto_10-008.mp3", aCtx, 1);
 
-let m2 = new Metro(aCtx, [200, 900, 900]);
+let m2 = new Metro(aCtx, [300, 200, 200, 200, 200]);
 
-sample2.getFile().then((samp) => {
+sample2.getFile().then((samp: Simple) => {
     m2.addEvery(samp);
-});
+}); */
 
-/* let n = new Noise(0.02);
+/* let n = new Noise(0.01);
 
 m2.addEvery(n);
  */
